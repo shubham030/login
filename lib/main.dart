369b612c8c617a78a091e12426ad8f061b24cb42
shubham <gshubham030@gmail.com>
2019-12-20 21:05:44 +0530

    updated launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Surfing',
+      title: 'Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
